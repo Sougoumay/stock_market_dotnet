e29dbc29b747468a42736d9b704efba934ee2396
Sougoumay <elidjaihamid@gmail.com>
2025-05-15 10:46:17 +0200

    Implémentation du controleur des stocks avec l'utilisation des mappers et des dtos

---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id {get; set;}
+        public string Title {get; set;} = string.Empty;
+        public string Content {get; set;} = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public int? StockId { get; set; } // La définition de la clé étrangère stockId
+
+        public Stock? Stock {get; set;} // Définition de la property de navigation, ce qui permet d'accéder au Stock depuis le commentaire.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Configurer la classe permettant de faire la connection à la BDD SqlServer
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
